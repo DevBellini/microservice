d57ac0e79f83ac2f8e010f2175735c365cde6d7a
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-04 14:27:09 -0300

    hr-payroll implementation mock

---
+++ payroll-microservice/src/main/java/com/devsuperior/payroll/microservice/services/PaymentService.java
@@ -0,0 +1,12 @@
+package com.devsuperior.payroll.microservice.services;
+
+import com.devsuperior.payroll.microservice.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+    public Payment getPayment(long workerId, int days){
+        return new Payment("bob", 200.0, days);
+    }
+
+}
