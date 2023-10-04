51a3ca51debdf7a098870c0d968a2cf7de3f2b76
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-04 14:58:06 -0300

    Feign Client

--- payroll-microservice/src/main/java/com/devsuperior/payroll/microservice/PayrollMicroserviceApplication.java
+++ payroll-microservice/src/main/java/com/devsuperior/payroll/microservice/PayrollMicroserviceApplication.java
@@ -2,7 +2,9 @@ package com.devsuperior.payroll.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PayrollMicroserviceApplication {
 
