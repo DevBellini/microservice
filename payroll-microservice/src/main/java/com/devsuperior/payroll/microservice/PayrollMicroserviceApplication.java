8f7dbc7cc3668dba927ae66690df723805725274
Guiherme Bellini <guilhermebellinigb@hotmail.com>
2023-10-04 13:53:38 -0300

    Project payroll created

---
+++ payroll-microservice/src/main/java/com/devsuperior/payroll/microservice/PayrollMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.payroll.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollMicroserviceApplication.class, args);
+	}
+
+}
---
+++ payroll-microservice/src/test/java/com/devsuperior/payroll/microservice/PayrollMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.payroll.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
